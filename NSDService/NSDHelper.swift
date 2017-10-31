381d69c555ca667df5420ec83ac0f73462057b79
LeeSangHoon <hun6728@ridi.com>
2017-10-31 18:43:00 +0900

    대략적인 프로토타입 완성 (연결은 되나, 메시지 전송은 아직)

--- NSDService/NSDHelper.swift
+++ NSDService/NSDHelper.swift
@@ -2,29 +2,55 @@
 import Foundation
 import MultipeerConnectivity
 
+protocol NSDHelperDelegate {
+    func receivedMsg(manager: NSDHelper, msg: String)
+    func connectedDevicesChanged(manager: NSDHelper, connectedDevices: [String])
+}
+
 class NSDHelper : NSObject {
     
+    public var delegate: NSDHelperDelegate?
     
     private let devicePeerId = MCPeerID(displayName: UIDevice.current.name)
     private let serviceAdvertiser: MCNearbyServiceAdvertiser
     private let serviceBrower: MCNearbyServiceBrowser
     
+    lazy var session : MCSession = {
+        let session = MCSession(peer: self.devicePeerId, securityIdentity: nil, encryptionPreference: .required)
+        session.delegate = self
+        return session
+    }()
+    
+    func send(sendMsg: String) {
+        if session.connectedPeers.count > 0 {
+            do {
+                try self.session.send(sendMsg.data(using: .utf8)!, toPeers: session.connectedPeers, with: .reliable)
+            } catch let error {
+                print("Error for sending: \(error)")
+            }
+        }
+    }
+    
     override init() {
-        self.serviceAdvertiser = MCNearbyServiceAdvertiser(peer: devicePeerId, discoveryInfo: nil, serviceType: "NSDService")
-        self.serviceBrower = MCNearbyServiceBrowser(peer: devicePeerId, serviceType: "NSDService")
-        super.init()
         
-        self.serviceAdvertiser.delegate = self
-        self.serviceAdvertiser.startAdvertisingPeer()
+        print("display name => ",devicePeerId.displayName)
+        
+        self.serviceAdvertiser = MCNearbyServiceAdvertiser(peer: devicePeerId, discoveryInfo: nil, serviceType: "NsdChat")
+        self.serviceBrower = MCNearbyServiceBrowser(peer: devicePeerId, serviceType: "NsdChat")
+        super.init()
         
         self.serviceBrower.delegate = self
         self.serviceBrower.startBrowsingForPeers()
-        
     }
     
     deinit {
         self.serviceAdvertiser.stopAdvertisingPeer()
         self.serviceBrower.stopBrowsingForPeers()
+    }
+    
+    func registerSetting() {
+        self.serviceAdvertiser.delegate = self
+        self.serviceAdvertiser.startAdvertisingPeer()
     }
 }
 
@@ -34,6 +60,7 @@ extension NSDHelper : MCNearbyServiceAdvertiserDelegate {
     }
     func advertiser(_ advertiser: MCNearbyServiceAdvertiser, didReceiveInvitationFromPeer peerID: MCPeerID, withContext context: Data?, invitationHandler: @escaping (Bool, MCSession?) -> Void) {
         print("did Receive Invitation From Peer \(peerID)")
+        invitationHandler(true, self.session)
     }
 }
 
@@ -48,5 +75,39 @@ extension NSDHelper: MCNearbyServiceBrowserDelegate {
     
     func browser(_ browser: MCNearbyServiceBrowser, foundPeer peerID: MCPeerID, withDiscoveryInfo info: [String : String]?) {
         print("found Peer => \(peerID)")
+        browser.invitePeer(peerID, to: self.session, withContext: nil, timeout: 20)
+    }
+}
+
+extension NSDHelper : MCSessionDelegate {
+    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+        if state == MCSessionState.connected{
+            print("connected!!!")
+            
+        } else if state == MCSessionState.connecting {
+            print("Connecting....")
+        } else if state == MCSessionState.notConnected {
+            print("Not Connected!!")
+        }
+        self.delegate?.connectedDevicesChanged(manager: self, connectedDevices:
+            session.connectedPeers.map{$0.displayName})
+    }
+    
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+        print("didReceiveData \(data)")
+        let str = String(data: data, encoding: .utf8)!
+        self.delegate?.receivedMsg(manager: self, msg: str)
+    }
+    
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+        print("didReceiveStream")
+    }
+    
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+        print("didStartReceivingResourceWithName")
+    }
+    
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
+        print("didFinishReceivingResourceWithName")
     }
 }
--- NSDService/ViewController.swift
+++ NSDService/ViewController.swift
@@ -5,21 +5,42 @@ class ViewController: UIViewController {
     private let nsdHelper = NSDHelper()
     @IBOutlet weak var showText: UITextView!
     @IBOutlet weak var inputText: UITextField!
+    @IBOutlet weak var connectBtn: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        nsdHelper.delegate = self 
     }
     
     @IBAction func registerBtnTapped(_ sender: Any) {
+        nsdHelper.registerSetting()
     }
     
     @IBAction func discoverBtnTapped(_ sender: Any) {
     }
     
-    @IBAction func connectBtnTapped(_ sender: Any) {
+    @IBAction func sendBtnTapped(_ sender: Any) {
+        showText.text = "\(showText.text)\nme : \(String(describing: inputText.text))"
+        nsdHelper.send(sendMsg: inputText.text!)
+    }
+}
+
+extension ViewController: NSDHelperDelegate {
+    func receivedMsg(manager: NSDHelper, msg: String) {
+        print("receivedMsg => ",msg)
+        showText.text = "\(showText.text!)\nthem : \(msg)"
     }
     
-    @IBAction func sendBtnTapped(_ sender: Any) {
+    func connectedDevicesChanged(manager: NSDHelper, connectedDevices: [String]) {
+        OperationQueue.main.addOperation {
+            print("connected Devices => ",connectedDevices)
+            if connectedDevices.count > 0 {
+                self.connectBtn.titleLabel?.text = "CONNECT"
+            } else {
+                self.connectBtn.titleLabel?.text = "DISCONNECT"
+            }
+        }
     }
+
 }
 
